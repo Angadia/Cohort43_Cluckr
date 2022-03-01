4c1a3b219687d2b7ab87901d778c02faddaed394
Jignesh Patel <CA.Jignesh.Angadia@gmail.com>
2022-03-01 13:25:49 -0800

    Create Clucks

--- routes/clucks.js
+++ routes/clucks.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const knex = require("../db/client");
 
 const router = express.Router();
 
@@ -7,6 +8,35 @@ router.get("/", (req, res) => {
     res.render("users/sign_in");
   } else {
     res.render("clucks/index");
+  }
+});
+
+router.get("/new", (req, res) => {
+  if (res.locals.username == "") {
+    res.render("users/sign_in");
+  } else {
+    res.render("clucks/new");
+  }
+});
+
+router.post("/", (req, res) => {
+  const content = req.body.content;
+  const image_url = req.body.image_url;
+  const username = req.cookies.username;
+
+  if (content.length > 0 || image_url.length > 0) {
+    knex("clucks")
+      .insert({
+        username: username,
+        content: content,
+        image_url: image_url,
+      })
+      .returning("*")
+      .then((cluck) => {
+        res.redirect("/");
+      });
+  } else {
+    res.redirect("clucks/new");
   }
 });
 
