05cf5ea93afb316f8143f107b7a54e6d03c358bc
Jignesh Patel <CA.Jignesh.Angadia@gmail.com>
2022-03-01 15:19:05 -0800

    Trending Tags

--- routes/clucks.js
+++ routes/clucks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const knex = require("../db/client");
-const TimeAgo = require('javascript-time-ago');
-const en = require('javascript-time-ago/locale/en');
+const TimeAgo = require("javascript-time-ago");
+const en = require("javascript-time-ago/locale/en");
 TimeAgo.addLocale(en);
 
 const router = express.Router();
@@ -10,12 +10,20 @@ router.get("/", (req, res) => {
   knex("clucks")
     .orderBy("created_at", "DESC")
     .then((clucks) => {
-      const timeAgo = new TimeAgo('en-CA');
+      const timeAgo = new TimeAgo("en-CA");
 
-      clucks.forEach(cluck => {
-        cluck['time_ago'] = timeAgo.format(cluck.created_at);
+      clucks.forEach((cluck) => {
+        cluck["time_ago"] = timeAgo.format(cluck.created_at);
       });
-      res.render("clucks/index", { clucks: clucks });
+      knex("hash_tags")
+        .select("hash_tag", "count")
+        .orderBy("count", "DESC")
+        .then((hashTagsCounts) => {
+          res.render("clucks/index", {
+            clucks: clucks,
+            trends: hashTagsCounts,
+          });
+        });
     });
 });
 
@@ -26,6 +34,74 @@ router.get("/new", (req, res) => {
     res.render("clucks/new");
   }
 });
+
+function createHashTags(newHashTags, res) {
+  if (newHashTags && newHashTags.length > 0) {
+    knex("hash_tags")
+      .insert({
+        hash_tag: newHashTags[0],
+        count: 1,
+      })
+      .returning("*")
+      .then((hashTagCount) => {
+        if (newHashTags.length > 1) {
+          return createHashTags(newHashTags.slice(1), res);
+        } else {
+          res.redirect("../");
+        }
+      });
+  } else {
+    res.redirect("../");
+  }
+}
+
+function updateHashTagsCounts(hashTagsCounts, newHashTags, res) {
+  if (hashTagsCounts && hashTagsCounts.length > 0) {
+    knex("hash_tags")
+      .where({ id: hashTagsCounts[0].id })
+      .update({ count: hashTagsCounts[0].count + 1 })
+      .returning("*")
+      .then((hashTagCount) => {
+        if (hashTagsCounts.length > 1) {
+          return updateHashTagsCounts(
+            hashTagsCounts.slice(1),
+            newHashTags,
+            res
+          );
+        } else {
+          return createHashTags(newHashTags, res);
+        }
+      });
+  } else {
+    return createHashTags(newHashTags, res);
+  }
+}
+
+function processHashTags(hashTags, res) {
+  knex("hash_tags")
+    .select("*")
+    .whereIn("hash_tag", hashTags)
+    .then((hashTagsCounts) => {
+      const uniqueHashTags = hashTags.reduce(
+        (x, y) => (x.includes(y) ? x : [...x, y]),
+        []
+      );
+      const newHashTags = [];
+
+      const existingHashTags =
+        hashTagsCounts && hashTagsCounts.length > 0
+          ? hashTagsCounts.map((hashTagCount) => hashTagCount.hash_tag)
+          : [];
+      if (uniqueHashTags) {
+        uniqueHashTags.map((hashTag) => {
+          if (!existingHashTags.includes(hashTag)) {
+            newHashTags.push(hashTag);
+          }
+        });
+      }
+      updateHashTagsCounts(hashTagsCounts, newHashTags, res);
+    });
+}
 
 router.post("/", (req, res) => {
   const content = req.body.content;
@@ -41,7 +117,19 @@ router.post("/", (req, res) => {
       })
       .returning("*")
       .then((cluck) => {
-        res.redirect("/");
+        // regex for hashtags
+        const re = /(^|)(#[a-z\d-]+)/gi;
+        const hashTags = [];
+        cluck[0].content.split(re).forEach((element) => {
+          if (element.length > 1 && element.startsWith("#")) {
+            hashTags.push(element.slice(1).toLowerCase());
+          }
+        });
+        if (hashTags.length > 0) {
+          processHashTags(hashTags, res);
+        } else {
+          res.redirect("../");
+        }
       });
   } else {
     res.redirect("clucks/new");
